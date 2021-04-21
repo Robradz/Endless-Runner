ff3eba567102f6b30fc4afe2618f12f84f6300da
Robert Radzville <rradzvil@ucsc.edu>
2021-04-20 18:15:17 -0700

    Initial commit with starting file structure

---
+++ src/main.js
@@ -0,0 +1,14 @@
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [Difficulty, PlayerCount, Multiplayer, Singleplayer],
+}
+
+let game = new Phaser.Game(config);
+
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
+// Declare Keys
+let keyA, keyD, keyW, keyS;
