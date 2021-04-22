0fafbef12efa727360328414ed9f20b459532553
Robert Radzville <rradzvil@ucsc.edu>
2021-04-22 10:32:47 -0700

    Added Menu.js, Play.js, adjusted main

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
-    scene: [Difficulty, PlayerCount, Multiplayer, Singleplayer],
+    scene: [Menu, Play]
 }
 
 let game = new Phaser.Game(config);
