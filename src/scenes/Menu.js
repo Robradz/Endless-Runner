03fa45eb6914ba1dc4fe85e49a66910fdf448070
Robert Radzville <rradzvil@ucsc.edu>
2021-04-24 14:57:28 -0700

    Creating menu screen. going to make a branch now

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,11 +8,37 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
-        this.scene.start('playScene');
+        // display score
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#03A1FC',
+            color: '#02112B',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
 
+        // Temporary Menu
+        //Show Menu Text
+        this.add.text(game.config.width/2, game.config.height/2, 'Dodgy Dino', 
+            menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 2, 
+            'Press W to start. Move with WASD', menuConfig).setOrigin(0.5);
+        
+        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
     }
 
     update() {
-        
+        if (Phaser.Input.Keyboard.JustDown(keyW)) {
+            // easy mode
+            game.settings = {
+                gameTimer: 60000
+            }
+            this.scene.start('playScene');
+        }
     }
 }
